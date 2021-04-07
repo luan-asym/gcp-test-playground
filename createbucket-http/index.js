08b0cc4b1bf36be34fccd46646fdbc307e80be03
Luan <ltran@asymmetrik.com>
2021-04-07 16:01:39 -0400

    fix: made whole function async

--- createbucket-http/index.js
+++ createbucket-http/index.js
@@ -6,29 +6,25 @@ const { Storage } = require('@google-cloud/storage');
  * @param {!express:Request} req HTTP request context.
  * @param {!express:Response} res HTTP response context.
  */
-exports.createBucket = (req, res) => {
+exports.createBucket = async (req, res) => {
   const LOCATION = 'US-EAST4';
   const STORAGE_CLASS = 'STANDARD';
-  const TOPIC = 'bucket-created';
+  const TOPIC = 'bucket-changed';
 
   const bucketName = req.body.bucketName;
 
   const storage = new Storage();
 
   // creates bucket
-  async function create() {
-    await storage.createBucket(bucketName, {
-      location: LOCATION,
-      storageClass: STORAGE_CLASS,
-    });
-    console.log(`Bucket ${bucketName} created!`);
+  await storage.createBucket(bucketName, {
+    location: LOCATION,
+    storageClass: STORAGE_CLASS,
+  });
+  console.log(`Bucket ${bucketName} created!`);
 
-    // creates notification
-    await storage.bucket(bucketName).createNotification(TOPIC);
-    console.log(`Bucket ${bucketName} registered for ${TOPIC} topic!`);
-  }
-
-  create().catch(console.error);
+  // creates notification for bucket
+  await storage.bucket(bucketName).createNotification(TOPIC);
+  console.log(`Bucket ${bucketName} registered for ${TOPIC} topic!`);
 
   res.status(200).send(`${bucketName} created and setup`);
 };
