9314e80be5dee2e1a66658e6fb7db83a62e4109d
Luan <ltran@asymmetrik.com>
2021-04-08 13:20:19 -0400

    added more logging?

--- onbucketchange-pubsub/index.js
+++ onbucketchange-pubsub/index.js
@@ -4,9 +4,12 @@
  * @param {object} message The Pub/Sub message.
  * @param {object} context The event metadata.
  */
-exports.onBucketChange = (message, context) => {
-  console.log(`Event: ${context.eventType}`);
-  console.log(`Bucket: ${context.bucketId}`);
-
+exports.onBucketChange = (message) => {
   console.log(`Message: ${Buffer.from(message.data, 'base64').toString()}`);
+
+  const attributes = message.attributes;
+
+  console.log(`Bucket: ${attributes.bucketId}`);
+  console.log(`Event: ${attributes.eventType}`);
+  console.log(`Object: ${attributes.objectId}`);
 };
