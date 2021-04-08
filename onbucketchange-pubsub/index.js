baba6ab34a880115cf18ead64089e12e216ba56f
Luan <ltran@asymmetrik.com>
2021-04-08 13:02:50 -0400

    fix: change params

--- onbucketchange-pubsub/index.js
+++ onbucketchange-pubsub/index.js
@@ -4,14 +4,6 @@
  * @param {object} message The Pub/Sub message.
  * @param {object} context The event metadata.
  */
-exports.onBucketChange = (message, context) => {
-  console.log(`Event ${context.eventId}`);
-  console.log(`  Event Type: ${context.eventType}`);
-  console.log(`  Bucket: ${file.bucket}`);
-  console.log(`  File: ${file.name}`);
-  console.log(`  Metageneration: ${file.metageneration}`);
-  console.log(`  Created: ${file.timeCreated}`);
-  console.log(`  Updated: ${file.updated}`);
-
+exports.onBucketChange = (message) => {
   console.log(`Message ${Buffer.from(message.data, 'base64').toString()}`);
 };
