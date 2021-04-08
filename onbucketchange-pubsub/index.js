e8447fc0690a83350bb977342b897dd604240764
Luan <ltran@asymmetrik.com>
2021-04-08 12:53:41 -0400

    feat: add onBucketChange

--- onbucketchange-pubsub/index.js
+++ onbucketchange-pubsub/index.js
@@ -1,10 +1,10 @@
 /**
  * Trigger for when bucket changes
  *
- * @param {object} file The Cloud Storage file metadata.
+ * @param {object} message The Pub/Sub message.
  * @param {object} context The event metadata.
  */
-exports.onBucketChange = (file, context) => {
+exports.onBucketChange = (message, context) => {
   console.log(`Event ${context.eventId}`);
   console.log(`  Event Type: ${context.eventType}`);
   console.log(`  Bucket: ${file.bucket}`);
@@ -12,4 +12,6 @@ exports.onBucketChange = (file, context) => {
   console.log(`  Metageneration: ${file.metageneration}`);
   console.log(`  Created: ${file.timeCreated}`);
   console.log(`  Updated: ${file.updated}`);
+
+  console.log(`Message ${Buffer.from(message.data, 'base64').toString()}`);
 };
