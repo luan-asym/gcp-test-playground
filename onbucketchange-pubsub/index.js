fa3ad08208dae9131520e776c6457b25026a9a1a
Luan <ltran@asymmetrik.com>
2021-04-08 13:13:35 -0400

    fix: add context

--- onbucketchange-pubsub/index.js
+++ onbucketchange-pubsub/index.js
@@ -4,6 +4,9 @@
  * @param {object} message The Pub/Sub message.
  * @param {object} context The event metadata.
  */
-exports.onBucketChange = (message) => {
-  console.log(`Message ${Buffer.from(message.data, 'base64').toString()}`);
+exports.onBucketChange = (message, context) => {
+  console.log(`Event: ${context.eventType}`);
+  console.log(`Bucket: ${context.bucketId}`);
+
+  console.log(`Message: ${Buffer.from(message.data, 'base64').toString()}`);
 };
