95f6e5bafd2c0519fdee27e2e74603b19f25489c
Luan <ltran@asymmetrik.com>
2021-04-09 11:20:45 -0400

    feat: add replace flag

--- transferFiles-http/index.js
+++ transferFiles-http/index.js
@@ -11,12 +11,9 @@ exports.transferFiles = async (req, res) => {
 
   const srcBucket = req.body.srcBucket || 'gcp-bucket-deposit';
   const destBucket = req.body.destBucket || 'gcp-bucket-destination';
+  const replace = req.body.delete || false;
 
   const files = await storage.bucket(srcBucket).getFiles();
-
-  files.forEach((file) => {
-    console.log(file.name);
-  });
 
   files.forEach(async (file) => {
     await storage
@@ -24,7 +21,9 @@ exports.transferFiles = async (req, res) => {
       .file(file.name)
       .copy(storage.bucket(destBucket).file(file.name));
 
-    console.log(`gs://${srcBucket}/${file} copied to gs://${destBucket}/${file}`);
+    if (replace) {
+      await storage.bucket(srcBucket).file(file.name).delete();
+    }
   });
 
   res.status(200).send(`${files.length} files moved from ${srcBucket} to ${destBucket}`);
