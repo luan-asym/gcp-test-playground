2887f8f949364e8198960339510ce7a9b5465bd0
Luan <ltran@asymmetrik.com>
2021-04-12 10:28:17 -0400

    debug: try arrays

--- formsubmit-http/index.js
+++ formsubmit-http/index.js
@@ -16,11 +16,12 @@ exports.processFormSubmit = (req, res) => {
 
   // [0] Create Bucket?
   // [1] Bucket Name
-  const [createBucket, bucketName, ...others] = responses;
 
-  console.info(createBucket);
-  console.info(bucketName);
-  console.info(others);
+  console.info(responses[0]);
+
+  // console.info(createBucket);
+  // console.info(bucketName);
+  // console.info(others);
 
   res.send(message);
 };
