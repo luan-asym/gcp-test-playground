bc1ca536ee05146ce3654f27f2f503ce375ad50b
Luan <ltran@asymmetrik.com>
2021-04-13 10:07:51 -0400

    feat: add axios

--- formsubmit-http/index.js
+++ formsubmit-http/index.js
@@ -1,3 +1,6 @@
+const axios = require('axios').default;
+const CREATE_BUCKET_URL = 'https://us-east4-gcp-testing-308520.cloudfunctions.net/createBucket';
+
 /**
  * Processes Google Form Submission
  *
@@ -14,17 +17,35 @@ exports.processFormSubmit = (req, res) => {
   console.info(`Timestamp: ${timestamp}`);
   console.info(`    Email: ${email}`);
 
-  // [0] Create Bucket?
-  // [1] Bucket Name
+  // [0] "Create Bucket?"
+  // [1] "Bucket Name"
   const [createBucket, bucketName, ...others] = responses;
 
   console.info(createBucket);
   console.info(bucketName);
 
+  // [2..4] Testing Questions
   const [Q1, Q2, Q3] = others;
   console.info(Q1);
   console.info(Q2);
   console.info(Q3);
 
-  res.send(message);
+  if (createBucket) {
+    axios
+      .post(CREATE_BUCKET_URL, {
+        bucketName: bucketName,
+      })
+      .then(
+        (res) => {
+          console.log(res);
+
+          res.send(`Bucket creation successful!`);
+        },
+        (err) => {
+          console.error(err);
+
+          res.status(400).send(`Bucket creation failed: ${err}`);
+        }
+      );
+  }
 };
