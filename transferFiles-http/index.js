10b75aec6e76bfacc8b3b068cf02b62aa4d24b1c
Luan <ltran@asymmetrik.com>
2021-04-28 11:42:24 -0400

    feat: add param checking

--- transferFiles-http/index.js
+++ transferFiles-http/index.js
@@ -9,24 +9,39 @@ const { Storage } = require('@google-cloud/storage');
 exports.transferFiles = async (req, res) => {
   const storage = new Storage();
 
-  const srcBucket = req.body.srcBucket || 'gcp-bucket-deposit';
-  const destBucket = req.body.destBucket || 'gcp-bucket-destination';
+  const srcBucketName = req.body.srcBucket || 'gcp-bucket-deposit';
+  const destBucketName = req.body.destBucket || 'gcp-bucket-destination';
   const deleteSrc = req.body.deleteSrc || false;
 
-  const [srcFiles] = await storage.bucket(srcBucket).getFiles();
+  // check if buckets exist
+  const srcBucket;
+  const srcBucketExists = await storage.bucket(srcBucketName).exists();
+  if (srcBucketExists[0]) {
+    srcBucket = storage.bucket(srcBucketName);
+  } else {
+    res.status(404).send(`srcBucket ${srcBucket} not found`);
+  }
 
-  console.log(`srcFiles: ${srcFiles}`);
+  const destBucket;
+  const destBucketExists = await storage.bucket(destBucketName).exists();
+  if (destBucketExists[0]) {
+    destBucket = storage.bucket(destBucketName);
+  } else {
+    res.status(404).send(`destBucket ${destBucket} not found`);
+  }
 
+  // iterate through all files in srcBucket
+  const [srcFiles] = await srcBucket.getFiles();
   srcFiles.forEach(async (file) => {
     console.log(`Processing ${file.name}...`);
 
     // check if file exists
-    const existsData = await storage.bucket(destBucket).file(file.name).exists();
+    const existsData = await destBucket.file(file.name).exists();
     if (existsData[0]) {
       console.log(`${file.name} already exists!`);
 
       // parse object metadata
-      const [metadata] = await storage.bucket(destBucket).file(file.name).getMetadata();
+      const [metadata] = await destBucket.file(file.name).getMetadata();
       const archivedName = `${file.name}-${metadata.timeCreated}`;
       const contentType = metadata.contentType;
 
@@ -35,23 +50,22 @@ exports.transferFiles = async (req, res) => {
       if (contentType != 'text/plain') {
         // archive file
         console.log(`Renaming ${file.name} to ${archivedName}`);
-        await storage.bucket(destBucket).file(file.name).rename(archivedName);
+        await destBucket.file(file.name).rename(archivedName);
       }
     }
 
     // copy over file
-    await storage
-      .bucket(srcBucket)
-      .file(file.name)
-      .copy(storage.bucket(destBucket).file(file.name));
+    await srcBucket.file(file.name).copy(destBucket.file(file.name));
 
     console.log(`${file.name} copied!`);
 
     if (deleteSrc) {
-      await storage.bucket(srcBucket).file(file.name).delete();
+      await srcBucket.file(file.name).delete();
       console.log(`${file} deleted!`);
     }
   });
 
-  res.status(200).send(`${srcFiles.length} file(s) moved from ${srcBucket} to ${destBucket}`);
+  res
+    .status(200)
+    .send(`${srcFiles.length} file(s) moved from ${srcBucketName} to ${destBucketName}`);
 };
