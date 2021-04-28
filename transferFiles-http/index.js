4c3a6d680b232af9cafcd702fadb0b6457671c58
Luan <ltran@asymmetrik.com>
2021-04-28 15:36:06 -0400

    ref: replace forEach with for await

--- transferFiles-http/index.js
+++ transferFiles-http/index.js
@@ -44,7 +44,7 @@ exports.transferFiles = async (req, res) => {
   // iterate through all files in srcBucket
   const [srcFiles] = await srcBucket.getFiles();
 
-  srcFiles.forEach(async (file) => {
+  for await (file of srcFiles) {
     console.log(`Processing ${file.name}...`);
 
     // check if file exists
@@ -75,7 +75,7 @@ exports.transferFiles = async (req, res) => {
       await srcBucket.file(file.name).delete();
       console.log(`${file} deleted!`);
     }
-  });
+  }
 
   res
     .status(200)
