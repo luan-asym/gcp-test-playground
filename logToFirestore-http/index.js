2c179e9dfc412980e321c6688fc1ab920e134987
Luan <ltran@asymmetrik.com>
2021-05-04 17:10:13 -0400

    feat: add firestore logging

--- logToFirestore-http/index.js
+++ logToFirestore-http/index.js
@@ -12,7 +12,29 @@ const TOPIC = 'firestore-log';
 exports.logToFirestore = async (req, res) => {
   const message = req.body;
 
+  const timestamp = message.timestamp;
+  const bucketName = message.bucketName;
+  const email = message.email;
+  const q1 = message.q1;
+  const q2 = message.q2;
+  const q3 = message.q3;
+
   console.log(`message: ${JSON.stringify(message)}`);
+
+  const document = firestore.doc(`bucket/${bucketName}`);
+
+  try {
+    await document.set({
+      timestamp: timestamp,
+      email: email,
+      q1: q1,
+      q2: q2,
+      q3: q3,
+    });
+    console.log(`Entry logged!`);
+  } catch (err) {
+    res.status(400).send(`Error: ${err.message}`);
+  }
 
   res.status(200).send('Sucessful run!');
 };
