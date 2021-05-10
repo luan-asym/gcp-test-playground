c1492715c63468f4f8f09f4d97d899d2ddc4727f
Luan <ltran@asymmetrik.com>
2021-05-10 18:02:33 -0400

    fix: update try-catch

--- onbucketchange-pubsub/index.js
+++ onbucketchange-pubsub/index.js
@@ -9,28 +9,28 @@ const FIRESTORE_LOG_TOPIC = 'firestore-log';
  * @param {object} context The event metadata.
  */
 exports.onBucketChange = async (psMessage) => {
-  const message = JSON.parse(Buffer.from(psMessage.data, 'base64').toString());
-  const attributes = psMessage.attributes;
-
-  console.log(`Message: ${JSON.stringify(message)}`);
-
-  // extract pubsub message data
-  const timestamp = attributes.eventTime;
-  const event = attributes.eventType;
-  const bucketName = attributes.bucketId;
-  const file = attributes.objectId;
-
-  // serialize data
-  const data = JSON.stringify({
-    bucketName: bucketName,
-    lastUpdateEvent: event,
-    lastUpdateFile: file,
-    lastUpdateTime: timestamp,
-  });
-  const dataBuffer = Buffer.from(data);
-
-  // update firestore entry with event data
   try {
+    const message = JSON.parse(Buffer.from(psMessage.data, 'base64').toString());
+    const attributes = psMessage.attributes;
+
+    console.log(`Message: ${JSON.stringify(message)}`);
+
+    // extract pubsub message data
+    const timestamp = attributes.eventTime;
+    const event = attributes.eventType;
+    const bucketName = attributes.bucketId;
+    const file = attributes.objectId;
+
+    // serialize data
+    const data = JSON.stringify({
+      bucketName: bucketName,
+      lastUpdateEvent: event,
+      lastUpdateFile: file,
+      lastUpdateTime: timestamp,
+    });
+    const dataBuffer = Buffer.from(data);
+
+    // update firestore entry with event data
     const pubSubClient = new PubSub();
 
     const firestoreLogMessageId = await pubSubClient.topic(FIRESTORE_LOG_TOPIC).publish(dataBuffer);
