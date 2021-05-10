4a52a6215ff4d1e04c0f631a785535ae731f077f
Luan <ltran@asymmetrik.com>
2021-05-10 10:27:33 -0400

    ref: remove stale code

--- logtofirestore-pubsub/index.js
+++ logtofirestore-pubsub/index.js
@@ -11,31 +11,12 @@ exports.logToFirestore = async (psMessage) => {
 
   console.log(`Message: ${JSON.stringify(message)}`);
 
-  // extract pubsub message data
-  const timestamp = message.timestamp;
-  const bucketName = message.bucketName;
-  const email = message.email;
-  const q1 = message.q1;
-  const q2 = message.q2;
-  const q3 = message.q3;
-
   // create client and get bucket collection
   const firestore = new Firestore();
   const collection = firestore.collection('bucket');
 
   // add firestore entry with form answers
   try {
-    // for (let [key, value] of Object.entries(message)) {
-    // }
-
-    const data = {
-      submissionTime: timestamp,
-      email: email,
-      Q1: q1,
-      Q2: q2,
-      Q3: q3,
-    };
-
     const document = await collection.doc(bucketName).set(message);
     console.log(`Document written at: ${document.writeTime.toDate()}`);
   } catch (err) {
