46c16945e65f85c983fcca2d9479aaa3245e2275
Luan <ltran@asymmetrik.com>
2021-05-11 16:15:53 -0400

    fix: revert to req.body

--- validator-http/index.js
+++ validator-http/index.js
@@ -8,7 +8,7 @@ const { Firestore } = require('@google-cloud/firestore');
  */
 exports.validator = async (req, res) => {
   try {
-    const message = Buffer.from(req.rawBody, 'base64').toString('ascii');
+    const message = req.body;
 
     console.log(`Keys: ${Object.keys(req)}`);
     console.log(`Body: ${JSON.stringify(message)}`);
